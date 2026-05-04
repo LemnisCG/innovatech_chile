86bdbf9324675136ab988abdb0002adaa3a9d57b
martin <ma.gauna@duocuc.cl>
2026-05-04 16:51:39 -0400

    feat: create RecursoRepository with custom query methods for filtering by user, project, and task IDs

--- resource-service/src/main/java/cl/innovatech/resourcemanagement/repository/RecursoRepository.java
+++ resource-service/src/main/java/cl/innovatech/resourcemanagement/repository/RecursoRepository.java
@@ -4,7 +4,11 @@ import cl.innovatech.resourcemanagement.entities.Recurso;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecursoRepository extends JpaRepository<Recurso, Long> {
-
+    List<Recurso> findByUsuarioId(Long usuarioId);
+    List<Recurso> findByIdProyecto(Long idProyecto);
+    List<Recurso> findByIdTarea(Long idTarea);
 }
